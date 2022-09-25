906a60820e778a0e45b9726bcc36f830db3aec56
Edwin gonzalez <emgonzalez050@gmail.com>
2022-09-25 12:44:43 -0500

    Aplicacion

---
+++ estudiantes/src/main/java/estudiantes/estudiantes/controller/StudentController.java
@@ -0,0 +1,21 @@
+package estudiantes.estudiantes.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+
+public class StudentController {
+@RequestMapping("/")
+    public String principal() {
+        return "create_student";
+}
+@RequestMapping("/students")
+    public String estudiantes() {
+        return "students";
+    }
+
+@RequestMapping("/editar")
+    public String editar() {
+        return "edit_student";
+    }
+}
---
+++ estudiantes/src/test/java/estudiantes/estudiantes/EstudiantesApplicationTests.java
@@ -0,0 +1,13 @@
+package estudiantes.estudiantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudiantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
